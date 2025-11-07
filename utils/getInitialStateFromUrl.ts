7e14cfe9766ce081524e384ac5021d892cfbba02
nb1507 <nb1507@jdwilliamsgroup.com>
2025-11-07 01:36:58 +0000

    inital commit

--- utils/getInitialStateFromUrl.ts
+++ utils/getInitialStateFromUrl.ts
@@ -3,17 +3,18 @@
  * Used for synchronous state initialization.
  */
 const getInitialStateFromUrl = (paramName: string): string[] => {
-    if (typeof window === 'undefined') {
-        return [];
-    }
-    const params = new URLSearchParams(window.location.search);
-    const paramValue = params.get(paramName);
-    if (paramValue) {
-        return paramValue.split(',')
-            .map(v => decodeURIComponent(v.trim()))
-            .filter(v => v.length > 0);
-    }
+  if (typeof window === "undefined") {
     return [];
-  };
+  }
+  const params = new URLSearchParams(window.location.search);
+  const paramValue = params.get(paramName);
+  if (paramValue) {
+    return paramValue
+      .split(",")
+      .map((v) => decodeURIComponent(v.trim()))
+      .filter((v) => v.length > 0);
+  }
+  return [];
+};
 
-  export default getInitialStateFromUrl
+export default getInitialStateFromUrl;
